401792d3a70eabe26f573ecd492a594feed9cba1
Kashif <dark_flash@outlook.com>
2015-09-13 03:16:09 -0400

    homescreen icons and layout

--- app/src/main/java/com/example/kz/obesityapp/MainActivity.java
+++ app/src/main/java/com/example/kz/obesityapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.kz.obesityapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button xml_whatpu = (Button) findViewById(R.id.button);
+        xml_whatpu.setOnClickListener(new View.OnClickListener() {
+     @Override
+              public void onClick(View v) {
+           // TODO Auto-generated method stub
+           startActivity(new Intent("android.intent.action.hm_screen"));
+
+            }
+              }
+        );
+        /////////////
     }
 
     @Override
---
+++ app/src/main/java/com/example/kz/obesityapp/hm_screen.java
@@ -0,0 +1,17 @@
+package com.example.kz.obesityapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by dark_ on 9/13/2015.
+ */
+public class hm_screen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homescreen);
+    }
+
+}
