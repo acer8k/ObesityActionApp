a0b8de52f9d3450779d69b1783957dadb0978036
Kashif <dark_flash@outlook.com>
2015-11-08 01:37:36 -0500

    minor changes

---
+++ app/src/main/java/com/example/kz/obesityapp/OverView.java
@@ -0,0 +1,21 @@
+package com.example.kz.obesityapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by dark_ on 10/11/2015.
+ */
+public class OverView extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.overview_screen);
+    }
+
+
+
+}
--- app/src/main/java/com/example/kz/obesityapp/hm_screen.java
+++ app/src/main/java/com/example/kz/obesityapp/hm_screen.java
@@ -1,10 +1,17 @@
 package com.example.kz.obesityapp;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  * Created by dark_ on 9/13/2015.
+ *
+ * // This is the homescreen
  */
 public class hm_screen extends Activity {
     @Override
@@ -12,6 +19,38 @@ public class hm_screen extends Activity {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homescreen);
-    }
 
+        Button xml_askfeed = (Button) findViewById(R.id.btn_over);
+        xml_askfeed.setOnClickListener(new View.OnClickListener() {
+
+             @Override
+             public void onClick(View v) {
+          // TODO Auto-generated method stub
+           startActivity(new Intent("android.intent.action.OverView"));
+             }
+             }
+        );
+    }
 }
+
+
+/*
+public class ContactPU extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.xml_contact);
+
+        Button xml_askfeed = (Button) findViewById(R.id.btnAsk);
+        xml_askfeed.setOnClickListener(new View.OnClickListener() {
+
+      @Override
+      public void onClick(View v) {
+      // TODO Auto-generated method stub
+      startActivity(new Intent("android.intent.action.AskFeedPU"));
+      }
+     }
+        );
+    }
+ */
